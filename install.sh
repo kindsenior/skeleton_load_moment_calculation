67722cf2702ac9a9486bcdbc178848d9b5eec3c9
kindsenior <kunio.einstein@gmail.com>
2020-05-23 17:37:35 +0900

    add installation of tex fonts

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 pip install pycddlib
+
+# setup tex fonts
+sudo apt install msttcorefonts -qq
+python3 -c "import matplotlib.font_manager; del matplotlib.font_manager.weight_dict['roman']; matplotlib.font_manager._rebuild()"
