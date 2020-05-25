4a118e006059d0a11306c1f7ea446d9e2b3b369b
kindsenior <kunio.einstein@gmail.com>
2020-05-25 21:19:54 +0900

    debug

--- src/structure_analyzer/structure_load_analyzer.py
+++ src/structure_analyzer/structure_load_analyzer.py
@@ -466,7 +466,7 @@ class JointLoadWrenchAnalyzer(object):
                                                     do_wait=do_wait,tm=tm)
 
         if joint_idx == 0:
-            logger.critical(Fore.YELLOW+re.search(r'([x-z][-_][x-z_-]+)', self.robot_model_file).group()+" max wrench: "+analyzer.array_str(self.max_load_wrench[3:])+Style.RESET_ALL)
+            logger.critical(Fore.YELLOW+re.search(r'([x-z][-_][x-z_-]+)', self.robot_model_file).group()+" max wrench: "+self.array_str(self.max_load_wrench[3:])+Style.RESET_ALL)
             if self.world.is_choreonoid:
                 self.tree_view.checkItem(self.robot_item, False)
                 self.message_view.flush()
