df255f8b9503efc7f2a84986aded339f4f2a8982
kindsenior <kunio.einstein@gmail.com>
2020-06-01 16:49:56 +0900

    add installation of imagematick and ffmpeg

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ pip install pycddlib
 # setup tex fonts
 sudo apt install msttcorefonts -qq
 python3 -c "import matplotlib.font_manager; del matplotlib.font_manager.weight_dict['roman']; matplotlib.font_manager._rebuild()"
+
+# for creating video
+sudo apt install imagemagick ffmpeg
