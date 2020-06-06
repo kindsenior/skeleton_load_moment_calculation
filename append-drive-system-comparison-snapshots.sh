abeaaa400b275ee871a8cf3dfeb3a0324a2f9ee3
kindsenior <kunio.einstein@gmail.com>
2020-06-06 18:34:14 +0900

    update video creating script

--- append-drive-system-comparison-snapshots.sh
+++ append-drive-system-comparison-snapshots.sh
@@ -1,6 +1,7 @@
 package_dir=${0%/*}
 
-crop_region="400x400+450+500"
+crop_region="620x620+200+0"
+size="90%"
 for region0 in $package_dir/drive-system-comparison/*system0_load-region_*.png
 do
     echo $region0
@@ -10,7 +11,7 @@ do
 
     pose0=${region0/load-region/pose}
     pose0_cut=${pose0/pose/pose_cut}
-    convert $pose0 -crop $crop_region -quality 10 -resize 200% $pose0_cut
+    convert $pose0 -crop $crop_region -quality 10 -resize $size $pose0_cut
     savefname0=${region0/_load-region/}
     echo $savefname0
     convert $pose0_cut -gravity center $region0 -append $savefname0
@@ -18,7 +19,7 @@ do
     region1=${region0/system0/system1}
     pose1=${region1/load-region/pose}
     pose1_cut=${pose1/pose/pose_cut}
-    convert $pose1 -crop $crop_region -quality 10 -resize 200% $pose1_cut
+    convert $pose1 -crop $crop_region -quality 10 -resize $size $pose1_cut
     savefname1=${region1/_load-region/}
     echo $savefname1
     convert $pose1_cut -gravity center $region1 -append $savefname1
@@ -26,7 +27,7 @@ do
     region2=${region0/system0/system2}
     pose2=${region2/load-region/pose}
     pose2_cut=${pose2/pose/pose_cut}
-    convert $pose2 -crop $crop_region -quality 10 -resize 200% $pose2_cut
+    convert $pose2 -crop $crop_region -quality 10 -resize $size $pose2_cut
     savefname2=${region2/_load-region/}
     echo $savefname2
     convert $pose2_cut -gravity center $region2 -append $savefname2
@@ -38,5 +39,7 @@ done
 echo "now converting to mp4"
 for idx in $(seq 0 2)
 do
-    ffmpeg -y -framerate 5 -i $package_dir/drive-system-comparison/drive-system-comparison_system${idx}_%02d.png -vcodec libx264 -pix_fmt yuv420p -r 30 $package_dir/drive-system-comparison/drive-system-comparison_system${idx}.mp4
+    ffmpeg -y -framerate 5 -i $package_dir/drive-system-comparison/drive-system-comparison_system${idx}_%02d.png\
+           -vcodec libx264 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"\
+           -r 30 $package_dir/drive-system-comparison/drive-system-comparison_system${idx}.mp4
 done
--- append-joint-configuration-comparison-snapshots.sh
+++ append-joint-configuration-comparison-snapshots.sh
@@ -4,10 +4,11 @@ for idx in $(seq 0 1)
 do
     pose=$package_dir/joint-configuration-comparison/joint-configuration-comparison_configuration${idx}_initial-pose.png
     pose_cut=${pose/pose/pose_cut}
-    convert $pose -crop "150x360+500+500" -quality 10 -resize 200% $pose_cut
+    convert $pose -crop "200x500+360+0" -quality 10 -resize 100% $pose_cut
 done
 
-crop_region="450x450+400+550"
+crop_region="700x700+200+0"
+size="90%"
 for region0 in $package_dir/joint-configuration-comparison/*configuration0_load-region_*.png
 do
     echo $region0
@@ -17,7 +18,7 @@ do
 
     pose0=${region0/load-region/pose}
     pose0_cut=${pose0/pose/pose_cut}
-    convert $pose0 -crop $crop_region -quality 10 -resize 200% $pose0_cut
+    convert $pose0 -crop $crop_region -quality 10 -resize $size $pose0_cut
     savefname0=${region0/_load-region/}
     echo $savefname0
     convert $pose0_cut -gravity center $region0 -append $savefname0
@@ -25,7 +26,7 @@ do
     region1=${region0/configuration0/configuration1}
     pose1=${region1/load-region/pose}
     pose1_cut=${pose1/pose/pose_cut}
-    convert $pose1 -crop $crop_region -quality 10 -resize 200% $pose1_cut
+    convert $pose1 -crop $crop_region -quality 10 -resize $size $pose1_cut
     savefname1=${region1/_load-region/}
     echo $savefname1
     convert $pose1_cut -gravity center $region1 -append $savefname1
@@ -36,5 +37,7 @@ done
 echo "now converting to mp4"
 for idx in $(seq 0 1)
 do
-    ffmpeg -y -framerate 5 -i $package_dir/joint-configuration-comparison/joint-configuration-comparison_configuration${idx}_%02d.png -vcodec libx264 -pix_fmt yuv420p -r 30 $package_dir/joint-configuration-comparison/joint-configuration-comparison_configuration${idx}.mp4
+    ffmpeg -y -framerate 5 -i $package_dir/joint-configuration-comparison/joint-configuration-comparison_configuration${idx}_%02d.png\
+           -vcodec libx264 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"\
+           -r 30 $package_dir/joint-configuration-comparison/joint-configuration-comparison_configuration${idx}.mp4
 done
